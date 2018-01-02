b37de1e7fc360c7277a52f25807184cf58f562b2
Harris-Kensington <34690827+Harris-Kensington@users.noreply.github.com>
2018-01-02 18:06:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    if (message.content === '?Test_Embed') {
+        const embed = new Discord.RichEmbed()
+        .setAuthor('Percy Bot', 'https://orig00.deviantart.net/9ce2/f/2014/201/1/7/profile_picture_by_alexthestooge-d7rk74y.png')
+        .setColor(0x00AE86)
+        .setDescription('This is the description of your embed message')
+        .setFooter('© WilliamJPercy#8066', 'https://orig00.deviantart.net/9ce2/f/2014/201/1/7/profile_picture_by_alexthestooge-d7rk74y.png')
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
